9d3fda2574d331510cde3e39e70215e66921d577
subasish123 <subasishkar35@gmail.com>
2022-03-26 19:40:07 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { ReactComponent as Logo } from "./logo.svg";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Logo style={{ height: 400 }} />
+        <h1>Welcome to my first react app</h1>
       </header>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,8 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // const linkElement = screen.getByText(/learn react/i);
+  // expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByText(/Welcome to my first react app/i);
+  expect(titleElement).toBeInTheDocument();
 });
